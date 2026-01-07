4af00e0d00a1de09253d37035a0f5baca82b5b53
djahasiel16 <djahasiel@gmail.com>
2026-01-07 16:11:09 +0800

    Fix: next js converstion

---
+++ types/radix-fixes.d.ts
@@ -0,0 +1,14 @@
+declare module '@radix-ui/react-tabs' {
+  const TabsPrimitive: any;
+  export = TabsPrimitive;
+}
+
+declare module '@radix-ui/react-scroll-area' {
+  const ScrollAreaPrimitive: any;
+  export = ScrollAreaPrimitive;
+}
+
+declare module '@radix-ui/react-switch' {
+  const SwitchPrimitive: any;
+  export = SwitchPrimitive;
+}
